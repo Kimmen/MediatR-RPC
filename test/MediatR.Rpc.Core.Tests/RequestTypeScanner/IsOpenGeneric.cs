ea4210de5d0f252ce037415c0f3e3b196058e7dd
Joakim Rosendahl <1217354+Kimmen@users.noreply.github.com>
2021-04-02 11:30:42 +0200

    Tests for type scanning.

---
+++ test/MediatR.Rpc.Core.Tests/RequestTypeScanner/IsOpenGeneric.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+using MediatR.Rpc.Core.Tests.Types;
+
+using Xunit;
+
+namespace MediatR.Rpc.Core.Tests.RequestTypeScanner
+{
+    public class IsOpenGeneric
+    {
+        [Fact]
+        public void GivenClosedGeneric_WhenEvaluating_ThenIsDeterminedNotToBeOpened()
+        {
+            var isOpenGeneric = Rpc.RequestTypeScanner.IsOpenGeneric(typeof(FlattenRequest));
+
+            isOpenGeneric.Should().BeFalse();
+        }
+
+        [Fact]
+        public void GivenOpenGeneric_WhenEvaluating_ThenIsDeterminedNotToBeOpened()
+        {
+            var isMediatrRequest = Rpc.RequestTypeScanner.IsOpenGeneric(typeof(OpenGenericRequest<>));
+
+            isMediatrRequest.Should().BeTrue();
+        }
+    }
+}
